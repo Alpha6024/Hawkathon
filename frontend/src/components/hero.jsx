18621ad4e697d8cdd1a1c2db357fc538603323a9
Pushkar Bhoge <pushkarbhoge@gmail.com>
2026-03-12 14:35:35 +0530

    second commit from Pushkar

--- frontend/src/components/hero.jsx
+++ frontend/src/components/hero.jsx
@@ -3,6 +3,7 @@ export default function hero(){
         <>
         <div className="h-[100vh] w-full flex justify-center items-center bg-blue-950">
             <div className="text-bold text-4xl text-white">WELCOME TO PRODUCTION</div>
+            <h1>Hi me Pushkar</h1>
         </div>
         </>
     )
