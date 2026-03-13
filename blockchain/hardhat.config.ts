e871544c580d46f20e1baf77e5e1048f54ff7017
Alpha6024 <pranavborkar40@gmail.com>
2026-03-13 07:41:38 +0530

    Add complete blockchain folder with hardhat configuration

---
+++ blockchain/hardhat.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "hardhat/config";
+import hardhatToolboxMochaEthers from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+
+export default defineConfig({
+  plugins: [hardhatToolboxMochaEthers],
+  solidity: { version: "0.8.28" },
+  networks: {
+    localhost: {
+      type: "http",
+      url: "http://127.0.0.1:8545",
+    },
+  },
+});
